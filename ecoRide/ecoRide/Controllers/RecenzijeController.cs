18d677696ace2204f0a3e10e9b16a30d3527d02d
nejrastr <86197301+nejrastr@users.noreply.github.com>
2022-06-14 22:21:14 +0200

    add

---
+++ ecoRide/ecoRide/Controllers/RecenzijeController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ecoRide.Data;
+using ecoRide.Models;
+
+namespace ecoRide.Controllers
+{
+    public class RecenzijeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RecenzijeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Recenzije
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Recenzije.Include(r => r.Korisnik);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Recenzije/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recenzije = await _context.Recenzije
+                .Include(r => r.Korisnik)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recenzije == null)
+            {
+                return NotFound();
+            }
+
+            return View(recenzije);
+        }
+
+        // GET: Recenzije/Create
+        public IActionResult Create()
+        {
+            ViewData["KorisnikId"] = new SelectList(_context.Korisnik, "Id", "Id");
+            return View();
+        }
+
+        // POST: Recenzije/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Ocjena,Komentar,KorisnikId")] Recenzije recenzije)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(recenzije);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["KorisnikId"] = new SelectList(_context.Korisnik, "Id", "Id", recenzije.KorisnikId);
+            return View(recenzije);
+        }
+
+        // GET: Recenzije/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recenzije = await _context.Recenzije.FindAsync(id);
+            if (recenzije == null)
+            {
+                return NotFound();
+            }
+            ViewData["KorisnikId"] = new SelectList(_context.Korisnik, "Id", "Id", recenzije.KorisnikId);
+            return View(recenzije);
+        }
+
+        // POST: Recenzije/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Ocjena,Komentar,KorisnikId")] Recenzije recenzije)
+        {
+            if (id != recenzije.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(recenzije);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecenzijeExists(recenzije.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["KorisnikId"] = new SelectList(_context.Korisnik, "Id", "Id", recenzije.KorisnikId);
+            return View(recenzije);
+        }
+
+        // GET: Recenzije/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recenzije = await _context.Recenzije
+                .Include(r => r.Korisnik)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recenzije == null)
+            {
+                return NotFound();
+            }
+
+            return View(recenzije);
+        }
+
+        // POST: Recenzije/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var recenzije = await _context.Recenzije.FindAsync(id);
+            _context.Recenzije.Remove(recenzije);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecenzijeExists(int id)
+        {
+            return _context.Recenzije.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ecoRide/ecoRide/Controllers/RezervacijaController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ecoRide.Data;
+using ecoRide.Models;
+
+namespace ecoRide.Controllers
+{
+    public class RezervacijaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RezervacijaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rezervacija
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Rezervacija.Include(r => r.Korisnik);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Rezervacija/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rezervacija = await _context.Rezervacija
+                .Include(r => r.Korisnik)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rezervacija == null)
+            {
+                return NotFound();
+            }
+
+            return View(rezervacija);
+        }
+
+        // GET: Rezervacija/Create
+        public IActionResult Create()
+        {
+            ViewData["KorisnikId"] = new SelectList(_context.Korisnik, "Id", "Id");
+            return View();
+        }
+
+        // POST: Rezervacija/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Vrijeme,KorisnikId")] Rezervacija rezervacija)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rezervacija);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["KorisnikId"] = new SelectList(_context.Korisnik, "Id", "Id", rezervacija.KorisnikId);
+            return View(rezervacija);
+        }
+
+        // GET: Rezervacija/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rezervacija = await _context.Rezervacija.FindAsync(id);
+            if (rezervacija == null)
+            {
+                return NotFound();
+            }
+            ViewData["KorisnikId"] = new SelectList(_context.Korisnik, "Id", "Id", rezervacija.KorisnikId);
+            return View(rezervacija);
+        }
+
+        // POST: Rezervacija/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Vrijeme,KorisnikId")] Rezervacija rezervacija)
+        {
+            if (id != rezervacija.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rezervacija);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RezervacijaExists(rezervacija.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["KorisnikId"] = new SelectList(_context.Korisnik, "Id", "Id", rezervacija.KorisnikId);
+            return View(rezervacija);
+        }
+
+        // GET: Rezervacija/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rezervacija = await _context.Rezervacija
+                .Include(r => r.Korisnik)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rezervacija == null)
+            {
+                return NotFound();
+            }
+
+            return View(rezervacija);
+        }
+
+        // POST: Rezervacija/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rezervacija = await _context.Rezervacija.FindAsync(id);
+            _context.Rezervacija.Remove(rezervacija);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RezervacijaExists(int id)
+        {
+            return _context.Rezervacija.Any(e => e.Id == id);
+        }
+    }
+}
