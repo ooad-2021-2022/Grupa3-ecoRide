5e015224d6d146593ec5977bd6834bc126a2ed4f
bzejnilovi1 <bzejnilovi1@etf.unsa.ba>
2022-06-15 15:53:21 +0200

    layout, kontrola pristupa

--- ecoRide/ecoRide/Controllers/PocetniZaslonController.cs
+++ ecoRide/ecoRide/Controllers/PocetniZaslonController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ecoRide.Data;
 using ecoRide.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ecoRide.Controllers
 {
+    [Authorize]
     public class PocetniZaslonController : Controller
     {
         private readonly ApplicationDbContext _context;
